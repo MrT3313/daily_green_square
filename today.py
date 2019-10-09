432ec25403410e4cd0686e3a8fee7eaa6e09bcbc
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-10-09 10:43:36 -0500

    Update today.py

--- today.py
+++ today.py
@@ -3,6 +3,6 @@ from datetime import date
 current_date = str(date.today())
 
 text_file = open("date_tracker.txt", "w")
-record = "Today is " + current_date + "\n"
+record = "Today is " + current_date + "\n" + "Have a good Day!"
 text_file.write(record)
 text_file.close()
